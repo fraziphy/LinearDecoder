5e066afa7a1d91ae73f8fff3bc736fc29769e161
fraziphy <farhad.razi.1988@gmail.com>
2025-02-21 18:42:39 +0100

    Reorganize project structure for installable module approach
    This commit focuses on restructuring the project directories without changing the core code content. The main changes are:
    
    - Reorganized directories for an installable module approach
    - Renamed and moved files to appropriate locations
    - Added LICENSE.txt file
    - Updated README.md with new structure information
    - Added Example_usage.ipynb for module usage demonstration
    
    Specific changes:
    - Moved LinearDecoder contents to src/lineardecoder/
    - Relocated scripts to examples/scripts/
    - Moved notebooks to examples/notebooks/
    - Added new LICENSE.txt (replacing old LICENSE file)
    - Updated README.md to reflect new structure
    - Added Example_usage.ipynb in examples/notebooks/
    
    This restructuring aims to improve the project's organization and make it more aligned with standard Python package structures.

--- LinearDecoder/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-# Import the config module from the current package
-from . import config
-
-# Import the funcs module from the current package
-from . import funcs
---
+++ src/lineardecoder/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# Import the class "LinearDecoder" from the funcs module from the current package
+from .funcs import LinearDecoder
